7fbb856b7c63b9644ceba56ea89930d1a6a1be73
Ahamoth <beeminer.store@gmail.com>
2023-01-24 19:23:21 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,15 @@
 import throttle from 'lodash.throttle';
-
 const form = document.querySelector('.feedback-form');
 const textarea = document.querySelector('.feedback-form textarea');
 const input = document.querySelector('.feedback-form input');
 const button = document.querySelector('.feedback-form button');
-
 const userForm = {
     email: '',
     message: '',
 };
-
-
 onSavedInfoReload();
-
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onTextereaInput, 500)); 
-
 function onFormSubmit(evt) {
     evt.preventDefault();
     if (userForm.email != '' && userForm.message != '') {
@@ -28,13 +22,10 @@ function onFormSubmit(evt) {
         alert("Заполните все поля!");
     }
 }
-
 function onTextereaInput(evt) {
     userForm[evt.target.name] = evt.target.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(userForm));
-    
 }
-
 function onSavedInfoReload() {
     const userWrited = localStorage.getItem("feedback-form-state");
     const userWritedBack = JSON.parse(userWrited);
@@ -43,14 +34,11 @@ function onSavedInfoReload() {
             input.value = userWritedBack.email;
             userForm.email = userWritedBack.email;
         } 
-    
         if (userWritedBack.message) {
             textarea.value = userWritedBack.message;
             userForm.message = userWritedBack.message;
         }   
     }
-     
-
 }
 
 
